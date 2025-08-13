037d7916ac5af355cadb6507aa41a468be5b9059
Olionnn <75076969+Olionnn@users.noreply.github.com>
2025-08-13 15:32:29 +0700

    sudah

--- src/pages/m_barang/BarangAtwJasa.jsx
+++ src/pages/m_barang/BarangAtwJasa.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useRef, useState, useCallback } from "react";
 import { MdFilterList, MdAdd, MdEdit, MdRefresh, MdSearch } from "react-icons/md";
+import { VscTrash } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import Modal from "../../component/Modal";
 import { useNavbar } from "../../hooks/useNavbar";
@@ -221,9 +222,15 @@ export default function BarangJasa() {
               </div>
             </div>
 
+              <div className="flex gap-2">
+              <button onClick={() => alert("Mau Hapus Items?")} className="inline-flex items-center gap-2 bg-white border border-red-500 text-red-700 px-3 py-2 rounded-lg hover:bg-green-50">
+                <VscTrash size={18} />
+              </button>
             <button onClick={handleOpenEdit} className="inline-flex items-center gap-2 bg-white border border-green-500 text-green-700 px-3 py-2 rounded-lg hover:bg-green-50">
               <MdEdit size={18} /> Edit
             </button>
+              </div>
+
                 </div>
 
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 text-base lg:text-lg">
